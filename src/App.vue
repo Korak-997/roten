cbddc475085b9657a3f0cc2b02d0a8c047377337
dani-lp <dani.linux.97@gmail.com>
2021-01-24 14:31:06 +0100

    Adding navbar in main element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
+  <nav-bar></nav-bar>
   <router-view />
 </template>
+
+<script>
+import Navbar from "./components/nav"
+export default {
+  name: "App",
+  components:{
+    "nav-bar": Navbar
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
