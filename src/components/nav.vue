877deaf77755a1bd902f9d5d4da38d9dc8cea53b
dani-lp <dani.linux.97@gmail.com>
2021-01-24 14:32:17 +0100

    Adding navbar elements

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="nav">
+    <ul>
+      <li><router-link v-bind:to="'/'">Home</router-link></li>
+      <li><router-link v-bind:to="'/about'">About</router-link></li>
+    </ul>
   </div>
 </template>
 
@@ -11,4 +15,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#nav{
+  width: 50%;
+  margin: 0 auto;
+  background-color: $secondary-black;
+  padding: .3em;
+  box-shadow: $shadow;
+  ul{
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    list-style: none;
+    li{
+      a{
+        text-decoration: none;
+        color: $dark-green;
+        font-size: 1.6rem;
+        &:hover{
+          color: $light-green;
+        }
+      }
+    }
+
+  }
+}
 </style>
