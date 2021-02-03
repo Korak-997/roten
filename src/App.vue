12a605221ec49de988589f48935501c36ba267cd
Korak Kurani <kurani@comcard.de>
2021-02-03 15:53:21 +0100

    Add styles to new Elements

--- src/App.vue
+++ src/App.vue
@@ -42,51 +42,24 @@ body{
   width: 100vw;
   height: 100vh;
 }
-#app{
-}
-.main{
-  margin: 2rem auto;
-  border: 2px solid rgb(199, 100, 64);
-  width: 90%;
-  height: 80%;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  padding: 2rem;
-}
-.left{
-  width: 30%;
-  display: flex;
-  flex-direction: column;
-}
-
-.right{
-  box-shadow: 3px 2px 10px 1px grey;
-  padding: 1rem;
+.action-box{
+  margin: 6em auto;
+  width: 50%;
+  text-align:center;
   color: snow;
-  text-align: center;
 }
 
-.input{
-  text-align: center;
-  background-color: transparent;
-  color: snow;
-  padding: 1rem;
-  border: none;
-  border-bottom: 2px solid rgb(199, 100, 64);
-  margin-bottom: 2rem;
-}
-
-.main button{
+button{
   background-color: rgb(199, 100, 64);
   border: none;
   border-radius: 2rem 1rem;
   color: snow;
   padding: 1rem;
   font-size: 1.5rem;
+  margin: 3em auto;
 }
 
-.main button:hover{
+button:hover{
   background-color: transparent;
   cursor: pointer;
   border: 2px solid rgb(199, 100, 64) ;
