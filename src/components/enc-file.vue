2cc39f95cb02d1b7ee6dae85023ace3785b6d3d6
Korak Kurani <kurani@comcard.de>
2021-02-04 09:59:45 +0100

    Add File Encryption component
    - New component is added for file encryption
      A file will be readed and encrypted based on given key
    - Only supports text files
    - adding stylings for the components

--- src/components/enc-file.vue
+++ src/components/enc-file.vue
@@ -1,106 +1,130 @@
 <template>
-  <div id="efile">
-    <div class="main">
-      <div class="left">
-        <h1>Encrypt file</h1>
-        <input class="input" v-model.lazy="text" type="text" placeholder="Write your text here.exp : Hello World">
-        <input class="input" v-model.lazy="key" type="number">
-        <button @click="encrypt">Encrypt</button>
-      </div>
-      <div v-show="encrypted" class="right">
-        <h3>Results</h3>
-        <hr>
-        <p>{{encrypted}}</p>
-      </div>
-    </div>
-  </div>
+	<div id="efile">
+		<div class="main">
+			<div class="left">
+				<p>Sorry now we only support txt files !</p>
+				<input
+					class="input"
+					@change="readFile"
+					id="file"
+					type="file"
+					accept="txt"
+				/>
+				<input class="input" v-model.lazy="key" type="number" />
+				<button @click="encrypt">Encrypt</button>
+			</div>
+			<div v-show="encrypted" class="right">
+				<h3>Results</h3>
+				<hr />
+				<p>{{ encrypted }}</p>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-  name: 'etext',
-  data(){
-    return{
-      encrypted : "",
-      text : "",
-      cLetters : "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''),
-      sLetters : "abcdefghijklmnopqrstuvwxyz".split(''),
-      key: 0
-    }
-  },
-  methods:{
-    encrypt(){
-      var arr = this.text.split('')
-      var k = parseInt(this.key, 10)
-      var enT = []
-      arr.forEach(l => {
-        if(l.match(/^[A-Za-z]+$/)){
-          if (l == l.toUpperCase()){
-            enT.push(this.cLetters[(this.cLetters.indexOf(l) + k)% this.cLetters.length])
-          }
-          else{
-            enT.push(this.sLetters[(this.sLetters.indexOf(l) + k)% this.sLetters.length])
-          }
-        }
-        else{
-          enT.push(l)
-        }
-      });
+	name: "etext",
+	data() {
+		return {
+			encrypted: "",
+			file: "",
+			text: "",
+			cLetters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+			sLetters: "abcdefghijklmnopqrstuvwxyz".split(""),
+			key: 0,
+		};
+	},
+	methods: {
+		encrypt() {
+			var arr = this.text.split("");
+			var k = parseInt(this.key, 10);
+			var enT = [];
+			arr.forEach((l) => {
+				if (l.match(/^[A-Za-z]+$/)) {
+					if (l == l.toUpperCase()) {
+						enT.push(
+							this.cLetters[
+								(this.cLetters.indexOf(l) + k) % this.cLetters.length
+							]
+						);
+					} else {
+						enT.push(
+							this.sLetters[
+								(this.sLetters.indexOf(l) + k) % this.sLetters.length
+							]
+						);
+					}
+				} else {
+					enT.push(l);
+				}
+			});
 
-      this.encrypted = enT.join('')
-      this.text = ""
-      this.key = 0
-    }
-
-  }
-}
+			this.encrypted = enT.join("");
+			this.text = "";
+			this.key = 0;
+			console.log(this.file);
+		},
+		readFile() {
+			const file = document.getElementById("file").files[0];
+			const reader = new FileReader();
+			reader.addEventListener("load", (e) => {
+				let t = e.target.result;
+				console.log(t);
+				this.text = t;
+			});
+			reader.readAsText(file);
+		},
+	},
+};
 </script>
 
 <style>
-.main{
-  margin: 2rem auto;
-  border: 2px solid rgb(199, 100, 64);
-  width: 90%;
-  height: 80%;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  padding: 2rem;
+.main {
+	margin: 2rem auto;
+	border: 2px solid rgb(199, 100, 64);
+	width: 90%;
+	height: 80%;
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	padding: 2rem;
+	color: snow;
 }
-.left{
-  width: 30%;
-  display: flex;
-  flex-direction: column;
-}
-
-.right{
-  box-shadow: 3px 2px 10px 1px grey;
-  padding: 1rem;
-  color: snow;
-  text-align: center;
+.left {
+	width: 30%;
+	display: flex;
+	flex-direction: column;
 }
 
-.input{
-  text-align: center;
-  background-color: transparent;
-  color: snow;
-  padding: 1rem;
-  border: none;
-  border-bottom: 2px solid rgb(199, 100, 64);
-  margin-bottom: 2rem;
+.right {
+	box-shadow: 3px 2px 10px 1px grey;
+	padding: 1rem;
+	color: snow;
+	text-align: center;
 }
 
-.main button{
-  background-color: rgb(199, 100, 64);
-  border: none;
-  border-radius: 2rem 1rem;
-  color: snow;
-  padding: 1rem;
-  font-size: 1.5rem;
+.input {
+	text-align: center;
+	background-color: transparent;
+	color: snow;
+	padding: 1rem;
+	border: none;
+	border-bottom: 2px solid rgb(199, 100, 64);
+	margin-bottom: 2rem;
 }
 
-.main button:hover{
-  background-color: transparent;
-  cursor: pointer;
-  border: 2px solid rgb(199, 100, 64) ;
+.main button {
+	background-color: rgb(199, 100, 64);
+	border: none;
+	border-radius: 2rem 1rem;
+	color: snow;
+	padding: 1rem;
+	font-size: 1.5rem;
+}
+
+.main button:hover {
+	background-color: transparent;
+	cursor: pointer;
+	border: 2px solid rgb(199, 100, 64);
 }
 </style>
